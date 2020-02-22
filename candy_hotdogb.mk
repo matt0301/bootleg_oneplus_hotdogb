b8abc5b56eec3a28aff90fa607772bd1ac9009b9
Dan Cartier <nospamdan1@gmail.com>
2020-02-21 19:09:01 -0500

    hotdogb: Build with pixelgapps
    Change-Id: Ieb6ee494ffc002d8e8d8da55b22a62557b41e808

--- candy_hotdogb.mk
+++ candy_hotdogb.mk
@@ -14,6 +14,9 @@ $(call inherit-product, device/oneplus/hotdogb/device.mk)
 # Inherit some common Candy stuff.
 $(call inherit-product, vendor/candy/config/common_full_phone.mk)
 
+# Inherit pixelgapps
+$(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := candy_hotdogb
 PRODUCT_DEVICE := hotdogb
